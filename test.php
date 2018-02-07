8baec1a6af73872c2e5532866bf5919d61c74b40
Colby Fox <colby@tachht.com>
2018-02-06 19:17:53 -0500

    no message

--- test.php
+++ test.php
@@ -1,7 +1,23 @@
-<?
+<?php
+//include_once('dump_r/dump_r.php');
 
-phpinfo();
+//phpinfo();
 
-echo "hello there!";
+//echo "hello there!";
+
+$test=24;
+
+//dump_r($test);
+
+//var_dump($test);
+
+//dump_r($_SERVER['DOCUMENT_ROOT']);
 
 ?>
+<script>
+
+
+alert('<?php echo $test; ?>');
+
+
+</script>
